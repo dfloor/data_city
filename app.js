429e49630a483f9c39834dd71e054a274eadbe15
daisuke tsunoda <dai@d-floor.net>
2017-10-30 04:47:41 +0900

    add layer

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ import {csv as requestCsv} from 'd3-request';
 const MAPBOX_TOKEN = "pk.eyJ1IjoiZGRsIiwiYSI6ImNqMjcxaDU1eTAwMnYycXBqdHU2eDF3NnEifQ.GmZGEAsreyN1qJ4Jfki8qQ"; // eslint-disable-line
 
 // Source data GeoJSON
-const DATA_URL = 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/geojson/vancouver-blocks.json'; // eslint-disable-line
-//const DATA_URL = './data/tokyo.geojson';  // eslint-disable-line
+//const DATA_URL = 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/geojson/vancouver-blocks.json'; // eslint-disable-line
+const DATA_URL = './data/tokyo.geojson';  // eslint-disable-line
 
 //
 const DATA_info = './data/BPO13101.csv'
+
+//
+const DATA_ROAD= './data/DB_RO.geojson';
 
 
 
@@ -33,14 +36,30 @@ class Root extends Component {
       },
       //data: null,
       //data1: null,
-      data: null
+      plotdata:null,
+      shapedata:null,
+      roaddata:null
     };
 
     requestJson(DATA_URL, (error, response) => {
       if (!error) {
-        this.setState({data: response});
+        this.setState({shapedata:response});
       }
     });
+
+    requestJson(DATA_ROAD, (error, response) => {
+      if (!error) {
+        this.setState({roaddata:response});
+      }
+    });
+
+    requestCsv(DATA_info, (error, response) => {
+      if (!error) {
+        //const data= response.map(d => ([Number(d.lang), Number(d.lat)]));
+        this.setState({plotdata:response});
+      }
+    });
+
 
   }
 
@@ -62,9 +81,10 @@ class Root extends Component {
     });
   }
 
+
   render() {
     //const {viewport,data1,data2} = this.state;
-    const {viewport,data} = this.state;
+    const {viewport,plotdata,shapedata,roaddata} = this.state;
 
     return (
       <MapGL
@@ -72,7 +92,9 @@ class Root extends Component {
         onViewportChange={this._onViewportChange.bind(this)}
         mapboxApiAccessToken={MAPBOX_TOKEN}>
         <DeckGLOverlay viewport={viewport}
-          data={data}
+          data1={shapedata}
+          data2={plotdata}
+          data3={roaddata}
           colorScale={colorScale}
           radius={30}
            />
@@ -81,73 +103,6 @@ class Root extends Component {
     );
   }
 }
-
-class Root1 extends Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        viewport: {
-          ...DeckGLOverlay.defaultViewport,
-          width: 500,
-          height: 500
-        },
-        //data: null,
-        //data1: null,
-        data: null
-      };
-  
-      requestJson(DATA_URL, (error, response) => {
-        if (!error) {
-          this.setState({data: response});
-        }
-      });
-  
-      requestCsv(DATA_info, (error, response) => {
-        if (!error) {
-          //const data= response.map(d => ([Number(d.lang), Number(d.lat)]));
-          this.setState({data:response});
-        }
-      });
-    }
-  
-    componentDidMount() {
-      window.addEventListener('resize', this._resize.bind(this));
-      this._resize();
-    }
-  
-    _resize() {
-      this._onViewportChange({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    }
-  
-    _onViewportChange(viewport) {
-      this.setState({
-        viewport: {...this.state.viewport, ...viewport}
-      });
-    }
-  
-    render() {
-      //const {viewport,data1,data2} = this.state;
-      const {viewport,data} = this.state;
-  
-      return (
-        <MapGL
-          {...viewport}
-          onViewportChange={this._onViewportChange.bind(this)}
-          mapboxApiAccessToken={MAPBOX_TOKEN}>
-          <DeckGLOverlay viewport={viewport}
-            data={data}
-            colorScale={colorScale}
-            radius={30}
-             />
-            
-        </MapGL>
-      );
-    }
-  }
   
 
 render(<Root />, document.body.appendChild(document.createElement('div'))
--- deckgl-overlay.js
+++ deckgl-overlay.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {setParameters} from 'luma.gl';
-import DeckGL, {GeoJsonLayer,ScatterplotLayer} from 'deck.gl';
+import DeckGL, {GeoJsonLayer,ScatterplotLayer,LineLayer} from 'deck.gl';
 
 const LIGHT_SETTINGS = {
   lightsPosition: [-125, 50.5, 5000, -122.8, 48.5, 8000],
@@ -24,54 +24,133 @@ export default class DeckGLOverlay extends Component {
     };
   }
 
+  constructor(props) {
+    super(props);
+    this.startAnimationTimer = null;
+    this.intervalTimer = null;
+    this.state = {
+      traffic: 0
+    };
+
+    this._startAnimate = this._startAnimate.bind(this);
+    this._animateHeight = this._animateHeight.bind(this);
+
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //if (nextProps.data.length !== this.props.data.length) {
+      this._animate();
+    //}
+  }
+
+  componentWillUnmount() {
+    this._stopAnimate();
+  }
+
+  _animate() {
+    this._stopAnimate();
+
+    // wait 1.5 secs to start animation so that all data are loaded
+    this.startAnimationTimer = window.setTimeout(this._startAnimate, 1500);
+  }
+
+  _startAnimate() {
+    this.intervalTimer = window.setInterval(this._animateHeight, 20);
+  }
+
+  _stopAnimate() {
+    window.clearTimeout(this.startAnimationTimer);
+    window.clearTimeout(this.intervalTimer);
+  }
+
+  _animateHeight() {
+    if (this.state.traffic === 24) {
+      this.setState({traffic:0});
+    } else {
+      this.setState({traffic: this.state.traffic + 0.1});
+    }
+    //console.log(this.state.traffic);
+  }
+
   _initialize(gl) {
     setParameters(gl, {
       depthTest: true,
       depthFunc: gl.LEQUAL
     });
   }
+  
 
   render() {
-    const {viewport,data, colorScale, radius} = this.props;
+    const {viewport,data1, data2, data3, colorScale, radius} = this.props;
 
-   if (!data) {
+   if (!data1) {
      return null;
    }
+   //console.log(data3);
 
-    const layer = new GeoJsonLayer({
-      id: 'geojson',
-      data,
-      opacity: 0.8,
-      stroked: false,
-      filled: true,
-      extruded: true,
-      wireframe: true,
-      fp64: true,
-      getElevation: f => (f.properties.nikken_a_8) * 4000*0.00054054,
-      getFillColor: f => colorScale((f.properties.nikken_a_8)/20.000),
-      getLineColor: f => [255, 255, 255],
-      lightSettings: LIGHT_SETTINGS,
-      pickable: Boolean(this.props.onHover),
-      onHover: this.props.onHover
-    });
-
-    if (!data) {
-      return null;
-    }
-
-    // const layer = new ScatterplotLayer({
-    //   id: 'scatter-plot',
-    //   data,
-    //   //radiusScale: radius,
-    //   radiusScale: 10,
-    //   radiusMinPixels: 0.25,
-    //   getPosition:d => [Number(d.lang), Number(d.lat),0],
-    //   getColor : d=> [(Number(d.buildingCode)/10000)*255,0,(1-(Number(d.buildingCode)/10000))*100],
-
+    // const layer1 = new GeoJsonLayer({
+    //   id: 'geojson',
+    //   data:data1,
+    //   opacity: 0.8,
+    //   stroked: false,
+    //   filled: true,
+    //   extruded: true,
+    //   wireframe: true,
+    //   fp64: true,
+    //   getElevation: f => (f.properties.nikken_a_8) * 4000*0.00054054,
+    //   getFillColor: f => colorScale((f.properties.nikken_a_8)/20.000),
+    //   getLineColor: f => [255, 255, 255],
+    //   lightSettings: LIGHT_SETTINGS,
+    //   pickable: Boolean(this.props.onHover),
+    //   onHover: this.props.onHover
     // });
 
+    // if (!data1) {
+    //   return null;
+    // }
+
+
+    const layer2 = new ScatterplotLayer({
+      id: 'scatter-plot',
+      data:data2,
+      //radiusScale: radius,
+      radiusScale: 10,
+      radiusMinPixels: 0.25,
+      getPosition:a => [Number(a.lang), Number(a.lat),0],
+      getColor : a=> [(Number(a.buildingCode)/10000)*255,0,(1-(Number(a.buildingCode)/10000))*100],
+
+    });
+    var traffic_prm = "heijitu_"+String(Math.floor(this.state.traffic));
+    //console.log(traffic_prm);
+
+
+    const layer3 = new GeoJsonLayer({
+      id: 'geojson',
+      data:data3,
+      opacity:1,
+      //opacity:f => eval("Number(f.properties.heijitu_"+Math.floor(this.state.traffic)+")/6000"),
+      stroked: true,
+      filled: false,
+      extruded: false,
+      wireframe: false,
+      fp64: false,
+      lineWidthScale:3,
+      //getElevation: f => (f.properties.nikken_a_8) * 4000*0.00054054,
+      //getFillColor: f => colorScale((f.properties.nikken_a_8)/20.000),
+      //getLineColor: f => [(Number(f.properties.heijitu_0)/2000)*255, 0, 0],
+      //getLineColor: f => eval("[(Number(f.properties.heijitu_"+Math.floor(this.state.traffic)+")/2000)*255, 0, 0]"),
+      getLineColor: f => [(this.state.traffic/24)*255, 0, 0],
+      //lightSettings: LIGHT_SETTINGS,
+      //pickable: Boolean(this.props.onHover),
+      //onHover: this.props.onHover
+    });
+
     return (
-      <DeckGL {...viewport} layers={ [layer] } onWebGLInitialized={this._initialize} />
+      <DeckGL {...viewport} layers={ [layer3] } onWebGLInitialized={this._initialize} />
     
       
     );
